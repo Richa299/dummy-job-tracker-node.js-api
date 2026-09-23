6b8ec22dff80844bfd613e625eef91c2a1d50dc8
Richa Singh <richa.singhnxt213@gmail.com>
2026-09-23 13:43:54 +0530

    use node.js file system for persistent data

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const jobs = [
-  { id: 1, title: "React Developer", company: "Adobe" },
-  { id: 2, title: "Frontend Developer", company: "Infosys" },
-];
+const fs = require("fs");
+
 const http = require("http");
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
@@ -21,79 +19,125 @@ const server = http.createServer((req, res) => {
   let id = 0;
 
   if (req.method == "GET" && req.url == "/jobs") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(jobs));
+    fs.readFile("jobs.json", "utf-8", (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end(
+          JSON.stringify({
+            message: "file not found",
+          }),
+        );
+        return;
+      }
+      const jobs = JSON.parse(data);
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(jobs));
+    });
   } else if (req.method == "POST" && req.url == "/jobs") {
     let body = "";
     req.on("data", (chunk) => {
       body = body + chunk.toString();
     });
-    req.on("end", () => {
+    fs.readFile("jobs.json", "utf-8", (err, data) => {
+      const jobs = JSON.parse(data);
+
       const job = JSON.parse(body);
       job.id = jobs.length + 1;
       jobs.push(job);
+      fs.writeFile("jobs.json", JSON.stringify(jobs), (err) => {
+        if (err) {
+          ((res.statusCode = 500),
+            res.end(JSON.stringify({ message: "Error saving job" })));
+        }
+      });
 
       res.statusCode = 201;
       res.setHeader("Content-Type", "application/json");
       res.end(JSON.stringify(job));
     });
-  } else if (req.method === "GET" && req.url.startsWith("/jobs/")) {
+  }
+  // else if (req.method === "GET" && req.url.startsWith("/jobs/")) {
+  //   parts = req.url.split("/");
+  //   id = Number(parts[parts.length - 1]);
+  //   let job = jobs.find((item) => item.id === id);
+  //   if (!job) {
+  //     res.statusCode = 404;
+  //     res.setHeader("Content-Type", "application/json");
+  //     res.end(
+  //       JSON.stringify({
+  //         message: "Job not found",
+  //       }),
+  //     );
+  //   } else {
+  //     res.statusCode = 200;
+  //     res.setHeader("Content-Type", "application/json");
+  //     res.end(JSON.stringify(job));
+  //   }
+  // }
+  else if (req.method === "DELETE" && req.url.startsWith("/jobs/")) {
     parts = req.url.split("/");
     id = Number(parts[parts.length - 1]);
-    let job = jobs.find((item) => item.id === id);
-    if (!job) {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "application/json");
-      res.end(
-        JSON.stringify({
-          message: "Job not found",
-        }),
-      );
-    } else {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(job));
-    }
-  } else if (req.method === "DELETE" && req.url.startsWith("/jobs/")) {
-    parts = req.url.split("/");
-    id = Number(parts[parts.length - 1]);
-    let index = jobs.findIndex((item) => item.id === id);
-    jobs.splice(index, 1);
-    if (index === -1) {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "application/json");
-      res.end(
-        JSON.stringify({
-          message: "Job not found",
-        }),
-      );
-    } else {
-      res.statusCode = 200;
-      res.end(JSON.stringify({ message: "Job deleted successfully" }));
-    }
-  } else if (req.method === "PUT" && req.url.startsWith("/jobs/")) {
-    parts = req.url.split("/");
-    id = Number(parts[parts.length - 1]);
-    const index = jobs.findIndex((item) => item.id === id);
+    fs.readFile("jobs.json", "utf-8", (err, data) => {
+      if (err) {
+        return;
+      }
+      const jobs = JSON.parse(data);
 
-    if (index === -1) {
-      res.statusCode = 404;
-      res.end("Job not found");
-      return;
-    } else {
-      let body = "";
-      req.on("data", (chunk) => {
-        body = body + chunk.toString();
-      });
-      req.on("end", () => {
+      let index = jobs.findIndex((item) => item.id === id);
+      jobs.splice(index, 1);
+      if (index === -1) {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "application/json");
+        res.end(
+          JSON.stringify({
+            message: "Job not found",
+          }),
+        );
+      } else {
+        fs.writeFile("jobs.json", JSON.stringify(jobs), (err) => {
+          if (err) {
+            req.statusCode = 500;
+            res.end("Error in deleting the job");
+            return;
+          }
+
+          res.statusCode = 200;
+          res.end(JSON.stringify({ message: "Job deleted successfully" }));
+        });
+      }
+    });
+  } else if (req.method === "PUT" && req.url.startsWith("/jobs/")) {
+    let body = "";
+    req.on("data", (chunk) => {
+      body = body + chunk.toString();
+    });
+    parts = req.url.split("/");
+    id = Number(parts[parts.length - 1]);
+    fs.readFile("jobs.json", "utf-8", (err, data) => {
+      const jobs = JSON.parse(data);
+
+      const index = jobs.findIndex((item) => item.id === id);
+
+      if (index === -1) {
+        res.statusCode = 404;
+        res.end("Job not found");
+        return;
+      } else {
         let updatedjob = JSON.parse(body);
         jobs[index] = { id: id, ...updatedjob };
-      });
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(jobs[index]));
-    }
+        fs.writeFile("jobs.json", JSON.stringify(jobs), (err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end("Error while updating the job");
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.end(JSON.stringify(jobs[index]));
+        });
+      }
+    });
   } else {
     res.statusCode = 400;
     res.end("route not found");
